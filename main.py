e4446517c9c7f66a11da11a2530c233d60739b4a
KaanNakipoglu <kaan.n@hotmail.com>
2022-06-08 22:42:57 +0300

    LAB9
    LAB9

---
+++ main.py
@@ -0,0 +1,111 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host="localhost",
+    user= "root",
+    passwd= "kaan1234"
+)
+cursorObject= database.cursor()
+cursorObject.execute("DROP DATABASE MyNewDatabase")
+
+cursorObject.execute("CREATE DATABASE MyNewDatabase")
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database = 'MyNewDatabase',
+    passwd="kaan1234"
+)
+if connection.is_connected():
+    db_Info = connection.get_server_info()
+    print("You are connected to the MySQL server", db_Info)
+    cursor = connection.cursor()
+    cursor.execute("select database() ")
+    record = cursor.fetchone()
+    print("You are connected to the database", record)
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        database='MyNewDatabase',
+        user='root',
+        password='kaan1234'
+    )
+    mySql_Create_Table_Query = """
+     CREATE TABLE Marvel(
+     ID int(10) NOT NULL,
+     MOVIE varchar(50) NOT NULL,
+     DATE varchar(10) NOT NULL,
+     MCUPHASE varchar(10))"""
+
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Create_Table_Query)
+    print("Students Table created successfully ")
+
+    cursor.execute("SHOW TABLES")
+    for table_name in cursor:
+        print(table_name)
+except mysql.connector.Error as error:
+    print("FAILED: {}".format(error))
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MYSQL is closed")
+
+fileObject = open("Marvel.txt")
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        database='MyNewDatabase',
+        user='root',
+        password='kaan1234'
+    )
+    CursorObject = connection.cursor()
+    while fileObject:
+        text = fileObject.readline()
+        if text == "":
+            break
+        splitLines = text.split()
+        Insert = """INSERT INTO marvel (ID, MOVIE, DATE, MCU_PHASE)
+                 VALUES (%s, %s, %s, %s)"""
+        record = (splitLines[0], splitLines[1], splitLines[2], splitLines[3])
+        CursorObject.execute(Insert, record)
+        connection.commit()
+    print("Records are added into Marvel table.")
+    CursorObject.close()
+
+    sql_select = "SELECT MOVIE FROM marvel"
+    cursorObject = connection.cursor()
+    cursorObject.execute(sql_select)
+    records = cursorObject.fetchall()
+    for row in records:
+        print(row)
+
+    sql_remove = "DELETE FROM marvel WHERE MOVIE= 'TheIncredibleHulk'"
+    cursorObject = connection.cursor()
+    cursorObject.execute(sql_remove)
+    connection.commit()
+
+    sql_list = "SELECT * FROM marvel WHERE MCU_PHASE='Phase2'"
+    cursorObject = connection.cursor()
+    cursorObject.execute(sql_list)
+    records2 = cursorObject.fetchall()
+    for row2 in records2:
+        print(row2)
+
+    sql_update = "UPDATE marvel SET DATE= 'November 3, 2017' WHERE MOVIE='Thor:Ragnarok'"
+    cursorObject = connection.cursor()
+    cursorObject.execute(sql_update)
+    connection.commit()
+
+except mysql.connector.Error as error:
+    print("Failed to insert record into marvel table {}".format(error))
+
+finally:
+    if connection.is_connected():
+        cursorObject.close()
+        connection.close()
+        print("MySQL connection is closed.")
